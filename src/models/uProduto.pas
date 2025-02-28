498bdf9610c3424cc8d2d067e7d461b42c8f19ff
Gustavo Sequeira <sequeira2003@gmail.com>
2025-02-28 16:41:44 -0300

    inclusao da classe de produtos

---
+++ src/models/uProduto.pas
@@ -0,0 +1,131 @@
+unit uProduto;
+
+interface
+
+uses
+  System.SysUtils;
+
+type
+  TProduto = class
+  private
+    FID: Integer;
+    FNome: string;
+    FDescricao: string;
+    FPreco: Currency;
+    FEstoque: Integer;
+    FCategoria: string;
+    FDataCadastro: TDateTime;
+
+    procedure SetID(const Value: Integer);
+    function GetID: Integer;
+
+    procedure SetNome(const Value: string);
+    function GetNome: string;
+
+    procedure SetDescricao(const Value: string);
+    function GetDescricao: string;
+
+    procedure SetPreco(const Value: Currency);
+    function GetPreco: Currency;
+
+    procedure SetEstoque(const Value: Integer);
+    function GetEstoque: Integer;
+
+    procedure SetCategoria(const Value: string);
+    function GetCategoria: string;
+
+    procedure SetDataCadastro(const Value: TDateTime);
+    function GetDataCadastro: TDateTime;
+
+  public
+    constructor Create;
+    property ID: Integer read GetID write SetID;
+    property Nome: string read GetNome write SetNome;
+    property Descricao: string read GetDescricao write SetDescricao;
+    property Preco: Currency read GetPreco write SetPreco;
+    property Estoque: Integer read GetEstoque write SetEstoque;
+    property Categoria: string read GetCategoria write SetCategoria;
+    property DataCadastro: TDateTime read GetDataCadastro write SetDataCadastro;
+  end;
+
+implementation
+
+{ TProduto }
+
+constructor TProduto.Create;
+begin
+  FDataCadastro := Now;
+end;
+
+function TProduto.GetID: Integer;
+begin
+  Result := FID;
+end;
+
+procedure TProduto.SetID(const Value: Integer);
+begin
+  FID := Value;
+end;
+
+function TProduto.GetNome: string;
+begin
+  Result := FNome;
+end;
+
+procedure TProduto.SetNome(const Value: string);
+begin
+  FNome := Value;
+end;
+
+function TProduto.GetDescricao: string;
+begin
+  Result := FDescricao;
+end;
+
+procedure TProduto.SetDescricao(const Value: string);
+begin
+  FDescricao := Value;
+end;
+
+function TProduto.GetPreco: Currency;
+begin
+  Result := FPreco;
+end;
+
+procedure TProduto.SetPreco(const Value: Currency);
+begin
+  FPreco := Value;
+end;
+
+function TProduto.GetEstoque: Integer;
+begin
+  Result := FEstoque;
+end;
+
+procedure TProduto.SetEstoque(const Value: Integer);
+begin
+  FEstoque := Value;
+end;
+
+function TProduto.GetCategoria: string;
+begin
+  Result := FCategoria;
+end;
+
+procedure TProduto.SetCategoria(const Value: string);
+begin
+  FCategoria := Value;
+end;
+
+function TProduto.GetDataCadastro: TDateTime;
+begin
+  Result := FDataCadastro;
+end;
+
+procedure TProduto.SetDataCadastro(const Value: TDateTime);
+begin
+  FDataCadastro := Value;
+end;
+
+end.
+
